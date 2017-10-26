1590a132459456e249be08c601c70e04c7d06dcc
github username <github email address>
2017-10-26 13:17:40 -0400

    added overlay pop up with txt

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ $(document).ready(function() {
   console.log("Hi, I'm loaded!")
   infoClick()
 
+  $('#my_popup').popup({
+    color: '#F65915',
+    opacity: 10.0
+  });
+
 })
 
 function infoClick(){
